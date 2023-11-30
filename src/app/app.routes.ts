467e313930f96544ba01864b3464eb1d1d64882e
KhurramSaeed700 <KhurramSaeed700>
2023-11-30 23:26:21 +0500

    angular playlist till part 7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        title:'Home Page'
+    },
+    {
+        path:'details/:id', //:id is just a placeholder for parameter value
+        component:DetailsComponent,
+        title:'Details Page'
+    }
+];
